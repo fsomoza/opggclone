d7a931f35f025a8b79d87766e11f114071d0763a
fsomoza <kiko_somotri@hotmail.es>
2022-07-02 03:10:20 +0200

    mi primr jsx

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import "./index.css";
+
+
+const app = <h1>Mi primera app de React</h1>;
+const container = document.getElementById("root");
+
+ReactDOM.render(app,container);
